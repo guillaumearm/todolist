1a01e6f114e199861209a0616b76e9f321b7d9af
trapcodien <trapcodien@hotmail.fr>
2016-01-23 23:22:15 +0100

    add some dependencies. TODO: gulp+webpack

--- public/app/main.js
+++ public/app/main.js
@@ -1,1 +1,35 @@
-console.log("Hello World");
+class TodoModel {
+	_setData(data) { 
+			if (!data.reduce) { data = [] }
+		this._data = data;
+		TodoModel.id = this._data.reduce((acc, {id}) => Math.max(acc,id), 0) + 1 || 1;
+	}
+
+	_syncCookie() {
+		document.cookie = JSON.stringify(this._data);
+	}
+
+	constructor() {
+		this._setData(JSON.parse(document.cookie));
+	}
+	get data() {
+		return this._data;
+	}
+	set data(jsonData) {
+		this._setData(jsonData);
+		this._syncCookie();
+	}
+};
+
+document.cookie = JSON.stringify([]);
+var test = new TodoModel();
+console.log("OK, Hello World !");
+
+var Test = React.createClass({
+	render: function() { return ( <h1>{this.props.children}</h1> ) }
+});
+
+ReactDOM.render(
+	<Test>Hello World, React.JS is OK.</Test>,
+	document.querySelector("#todoAppContent")
+);
