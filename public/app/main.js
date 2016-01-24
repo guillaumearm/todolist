5726fc25aac5a47790c5974b6ace08f0be11edd3
trapcodien <trapcodien@hotmail.fr>
2016-01-24 02:12:30 +0100

    save.

--- public/app/main.js
+++ public/app/main.js
@@ -1,12 +1,20 @@
 class TodoModel {
 	_setData(data) { 
-			if (!data.reduce) { data = [] }
+			if (!data.reduce) { data = [] ; }
 		this._data = data;
-		TodoModel.id = this._data.reduce((acc, {id}) => Math.max(acc,id), 0) + 1 || 1;
+		TodoModel.id = (this._data.reduce((acc, {id}) => Math.max(acc,id), (-1)) + 1) || 0;
 	}
 
 	_syncCookie() {
 		document.cookie = JSON.stringify(this._data);
+	}
+
+	_setTodoState(id, state) {
+		var i = this._data.map(e => e.id).indexOf(id);
+		if (i >= 0) {
+			this._data[i].done = state;
+			this._syncCookie();
+		}
 	}
 
 	constructor() {
@@ -19,17 +27,61 @@ class TodoModel {
 		this._setData(jsonData);
 		this._syncCookie();
 	}
+
+	add(content) {
+		this._data.push({
+			id: TodoModel.id++,
+			content: content,
+			date: new Date(),
+			done: false
+		});
+		this._syncCookie();
+	}
+
+	del(id) {
+		var i = this._data.map(e => e.id).indexOf(id);
+		if (i >= 0) {
+			this._data.splice(i, 1)
+			this._syncCookie();
+		}
+	}
+
+	update(id, content) {
+		var i = this._data.map(e => e.id).indexOf(id);
+		if (i >= 0) {
+			this._data[i].content = content ; this._data[i].date = new Date()
+			this._syncCookie();
+		}
+	}
+
+	done(id) {
+		this._setTodoState(id, true);
+	}
+
+	undo(id) {
+		this._setTodoState(id, false);
+	}
 };
 
 document.cookie = JSON.stringify([]);
-var test = new TodoModel();
-console.log("OK, Hello World !");
+var todo = new TodoModel();
+todo.add("Hello World.");
+todo.add("Hello World 2.");
+todo.update(0, "YOLO");
+todo.done(0);
+todo.done(1);
+todo.del(0);
+todo.del(1);
 
 var Test = React.createClass({
-	render: function() { return ( <h1>{this.props.children}</h1> ) }
+	render: function() { 
+		var x = "NOP"
+		if (this.props.children[0]) { var x = this.props.children[0].content }
+		return ( <h1>{x}</h1> )
+	}
 });
 
 ReactDOM.render(
-	<Test>Hello World, React.JS is OK.</Test>,
+	<Test>{todo.data}</Test>,
 	document.querySelector("#todoAppContent")
 );
