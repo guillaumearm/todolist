ee30d26ea8e684ea57ff40395497cd10db011e2a
Guillaume ARM <guillaume.arm@redpelicans.com>
2016-02-23 22:28:50 +0100

    re-indent constant/Todos

--- app/constants/Todos.js
+++ app/constants/Todos.js
@@ -1,23 +1,23 @@
 import { PropTypes } from 'react'
 
 // Actions types
-export const ADD 			= 'ADD'
-export const DELETE 			= 'DELETE'
-export const EDIT 			= 'EDIT'
-export const DO 			= 'DO'
-export const UNDO 			= 'UNDO'
-export const EDITING_STATE		= 'EDITING_STATE'
+export const ADD 						= 'ADD'
+export const DELETE 				= 'DELETE'
+export const EDIT 					= 'EDIT'
+export const DO 						= 'DO'
+export const UNDO 					= 'UNDO'
+export const EDITING_STATE	= 'EDITING_STATE'
 export const EDITED_STATE		= 'EDITED_STATE'
 export const SET_FOCUS			= 'SET_FOCUS'
 
 // Actions generators propTypes
 export const propTypes			= PropTypes.shape({
-	setFocus:		PropTypes.func.isRequired,
-	add:			PropTypes.func.isRequired,
-	del:			PropTypes.func.isRequired,
-	edit:			PropTypes.func.isRequired,
+	setFocus:				PropTypes.func.isRequired,
+	add:						PropTypes.func.isRequired,
+	del:						PropTypes.func.isRequired,
+	edit:						PropTypes.func.isRequired,
 	editContent:		PropTypes.func.isRequired,
-	doIt:			PropTypes.func.isRequired,
-	setEditing:		PropTypes.func.isRequired,
-	setEdited:		PropTypes.func.isRequired,
+	doIt:						PropTypes.func.isRequired,
+	setEditing:			PropTypes.func.isRequired,
+	setEdited:			PropTypes.func.isRequired,
 })
