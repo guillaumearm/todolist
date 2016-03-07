9fd1e86aeb6021588f59ba6787b20aaab0febe5e
Guillaume ARM <guillaume.arm@redpelicans.com>
2016-03-07 17:49:27 +0100

    save

---
+++ app/app.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+
+export default props => {
+  return (
+    <div><p>Hello World</p></div>
+  )
+}
+
--- app/main.js
+++ app/main.js
@@ -5,23 +5,10 @@ import './css/base.css'
 import React from 'react'
 import { render } from 'react-dom'
 
-// Redux
-import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
-
 // App
-import App from './containers/App'
-
-// Reducers
-import rootReducer from './reducers'
-
-// initialState
-import { initialState } from './constants/TodoSkel'
+import App from './app'
 
 render (
-	<Provider store={createStore(rootReducer, [initialState], applyMiddleware(thunk))}>
 		<App />
-	</Provider>
 	, document.getElementById('todoAppContent')
 )
